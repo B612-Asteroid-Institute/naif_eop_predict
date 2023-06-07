0415136c326949fc52769742249d94082f623048
Joachim Moeyens <moeyensj@gmail.com>
2023-06-06 17:56:03 -0700

    Release v2023.06.03

--- naif_eop_predict/__init__.py
+++ naif_eop_predict/__init__.py
@@ -1,4 +1,4 @@
 from importlib.resources import files
 
-eop_predict = files("naif_eop_predict").joinpath("earth_200101_990628_predict.bpc").as_posix()
-_eop_predict_md5 = files("naif_eop_predict").joinpath("earth_200101_990628_predict.md5").as_posix()
+eop_predict = files("naif_eop_predict").joinpath("earth_200101_990825_predict.bpc").as_posix()
+_eop_predict_md5 = files("naif_eop_predict").joinpath("earth_200101_990825_predict.md5").as_posix()
